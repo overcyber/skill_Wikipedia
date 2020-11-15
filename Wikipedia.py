35ca0b97bbe3aef2ffbde0db87d0d2b40052abf1
LazzaAU <larry@fulltimecaravanning.com.au>
2020-11-15 13:50:56 +1000

    :sparkles: :bug: Update wikipedia skill to use a  updated module

--- Wikipedia.py
+++ Wikipedia.py
@@ -1,10 +1,9 @@
-from wikipedia import wikipedia
+from mediawikiapi import MediaWikiAPI
 
 from core.base.model.Intent import Intent
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler, Online
-from core.ProjectAliceExceptions import OfflineError
 
 
 class Wikipedia(AliceSkill):
@@ -12,6 +11,15 @@ class Wikipedia(AliceSkill):
 	Author: Psychokiller1888
 	Description: Allows one to find informations about a topic on wikipedia
 	"""
+
+
+	def __init__(self):
+		self._resultSummary = ""
+		self._top5Results = list()
+		self._alternatveResultUsed = False
+		self._devDebug = False
+		super().__init__()
+
 
 	@staticmethod
 	def _extractSearchWord(session: DialogSession) -> str:
@@ -25,9 +33,9 @@ class Wikipedia(AliceSkill):
 		self.continueDialog(
 			sessionId=session.sessionId,
 			text=self.randomTalk(text=question, replace=[search]),
-			intentFilter=[Intent('UserRandomAnswer'), Intent('SpellWord')],
+			intentFilter=[Intent('UserRandomAnswer')],  # Intent('SpellWord')],
 			currentDialogState='whatToSearch',
-			probabilityThreshold=0.05
+			probabilityThreshold=0.01
 		)
 
 
@@ -36,24 +44,118 @@ class Wikipedia(AliceSkill):
 	@IntentHandler('SpellWord', requiredState='whatToSearch')
 	@Online
 	def searchIntent(self, session: DialogSession):
-		search = self._extractSearchWord(session)
+		if 'UserRandomAnswer' in session.intentName:
+			search = session.payload['input']
+		else:
+			search = self._extractSearchWord(session)
+
 		if not search:
 			self._whatToSearch(session, 'whatToSearch')
 			return
 
-		wikipedia.set_lang(self.LanguageManager.activeLanguage)
-		try:
-			result = wikipedia.summary(search, sentences=3)
-		except wikipedia.DisambiguationError as e:
-			self.logWarning(msg='Ambiguous result')
-			self._whatToSearch(session, 'ambiguous')
-		except wikipedia.WikipediaException as e:
-			self.logWarning(msg='No match')
+		mediawikiapi = MediaWikiAPI()
+		# set language of the results
+		mediawikiapi.config.language = self.LanguageManager.activeLanguage
+		if self._devDebug:
+			self.logInfo(f'User request = {search}')
+		# Store the top 5 titles of the requested search (5 reduces chances of index error)
+		self._top5Results = mediawikiapi.search(search, results=5)
+		# set a index value for iterating through a list in the dialogs
+		index = 0
+
+		if not self._top5Results:
+			self.logWarning('No match')
 			self._whatToSearch(session, 'noMatch')
-		except Exception as e:
-			self.logWarning(msg=str(e), printStack=True)
+			return
+
+		# remove known ambiguous results
+		self.removeKnowenAmbiguousResults()
+
+		# Check for exceptions and return any good result
+		self._resultSummary = self.sortThroughResults(wikiInstance=mediawikiapi, index=index)
+
+		# If there are bo good answers then log a warning and inform user
+		if not self._resultSummary:
+			self.logWarning('No match')
+			self._whatToSearch(session, 'noMatch')
+
+		# If search result found say the result and end
 		else:
-			if not result:
-				self._whatToSearch(session, 'noMatch')
+			if self._devDebug:
+				self.logInfo(f'result Summary is {self._resultSummary}')
+
+			if not self._alternatveResultUsed:
+				self.sayResult(session=session, index=index)
 			else:
-				self.endDialog(sessionId=session.sessionId, text=result)
+				self.sayAlternatives(alternatives=self._top5Results[index + 1])
+				self.sayResult(session=session, index=index)
+
+
+	def removeKnowenAmbiguousResults(self):
+		"""Remove knowen ambiguos results and leave potential results"""
+
+		for item in self._top5Results:
+			if '(disambiguation)' in item:
+				self._top5Results.remove(item)
+		if self._devDebug:
+			self.logInfo(f'Top 5 results = {self._top5Results}')
+			self.logInfo("")
+
+
+	def sortThroughResults(self, wikiInstance, index):
+		errorMsg = ""
+		resultSummary = ""
+		while index <= 4:
+			try:
+				resultSummary = wikiInstance.summary(self._top5Results[index], sentences=self.getConfig('maxSentences'))
+			except Exception as e:
+				errorMsg = str(e)
+			finally:
+				if 'may refer to' in resultSummary or 'Try another id' in errorMsg:
+					self._alternatveResultUsed = True
+					index += 1
+				else:
+					index = 5
+
+			if errorMsg:
+				self.logWarning(f'Skipping over this error message {errorMsg}')
+
+		return resultSummary
+
+
+	def sayAlternatives(self, alternatives: str):
+		self.say(
+			text=self.randomTalk(text="dialogMessage1", replace=[alternatives]),
+			canBeEnqueued=False
+		)
+
+
+	def sayResult(self, session, index):
+		self.say(
+			text=self._resultSummary
+		)
+		self.endSession(sessionId=session.sessionId)
+		if self._alternatveResultUsed:
+			index += 2
+		index += 1
+		if index <= len(self._top5Results):
+			message = f'You may also be interested in {self._top5Results[index]}'
+			self.ThreadManager.doLater(
+				interval=3,
+				func=self.delayedSayMessage,
+				kwargs={'message': message}
+			)
+
+		self._resetObjects()
+
+
+	def _resetObjects(self):
+		self._resultSummary = ""
+		self._top5Results = list()
+		self._alternatveResultUsed = False
+
+
+	def delayedSayMessage(self, message):
+		self.say(
+			text=message
+		)
--- instructions/en.md
+++ instructions/en.md
@@ -2,16 +2,32 @@
 
 Some examples of what to say : 
 
+### NOTE: This method works best for single word searches like "search wiki for info on cheese"
 - Hey Alice/Snips
 
 - "i need some online info about finland"
-- "get me information from wikipedia about Donald Trump"
+- "get me information from wikipedia about Trump"
 - "can you do a search about nuclear weapons on wiki please"
 - "could you search some info online",
-- "search wiki about rubber bands",
+- "search wiki about apple",
 - "search wiki for info about Switzerland"
+- "search wiki for electricity"
 
-or simply "search wikipedia"... when Alice asks "sure what would you like me search for" ? 
-Either say your search item or you can also spell the word to her that you want
-to search for 
+### Note This below method works better for multi word searches, like "search wiki for info on "blue cheese"
+
+or simply say 
+
+- "search wikipedia"
+ 
+when Alice asks
+ 
+ - "sure what would you like me search for" ? 
+
+Either say your search item {EG: Blue cheese"} or you can also spell the word to her that you want
+to search for.
+
+## Skill settings
+
+Select how many sentences you want returned by going to the skill settings and adjusting "maxSentences" value
+default is 2
 
